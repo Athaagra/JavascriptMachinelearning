1090f1f77748e64c8fccca65cd67be2dd0096646
Athanasios Agrafiotis <46803277+Athaagra@users.noreply.github.com>
2024-06-04 05:45:50 +0300

    Create linearRegression.js

---
+++ linearRegression.js
@@ -0,0 +1,26 @@
+class LinearRegression{
+	constructor(features, labels, options){
+		this.features = features;
+		this.labels = labels;
+		
+		this.options = Object.assign(
+			{ learningRate: 0.1, iterations: 1000},
+			options
+		);
+		this.m=0;
+		this.b=0;
+	}
+	
+	gradeientDescent(){
+		const currentGuessesForMPG = this.features.map(row => {
+			return this.m * row[0] + this.b;
+		});
+	
+	}
+	
+	train() {
+		for (let i=0; i < this.options.iterations; i++){
+			this.gradientDescent();
+		}
+	}	
+}
